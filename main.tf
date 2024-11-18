3c51e2f820273b75935c3a82489b60dbf817d870
Danny Keizer <danny.keizer@protonmail.com>
2024-11-18 14:04:31 +0100

    Fix: Added blocks

--- main.tf
+++ main.tf
@@ -13,3 +13,13 @@ module "local_create_files" {
   source = "git::https://github.com/danny-keizer/terraform-modules.git//Modules//Local_Files"
   file_name = "DEV"
 }
+
+module "local_create_files" {
+  source = "git::https://github.com/danny-keizer/terraform-modules.git//Modules//Local_Files"
+  file_name = "ACC"
+}
+
+module "local_create_files" {
+  source = "git::https://github.com/danny-keizer/terraform-modules.git//Modules//Local_Files"
+  file_name = "PROD"
+}
