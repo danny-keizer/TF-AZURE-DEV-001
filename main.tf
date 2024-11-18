714eba76416ed744e3f8e25b0fa2c0e50d30b263
Danny <85065163+danny-keizer@users.noreply.github.com>
2024-11-18 13:41:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform { 
+  cloud { 
+    
+    organization = "private-acme" 
+
+    workspaces { 
+      name = "ci-demo" 
+    } 
+  } 
+}
+
 module "local_create_files" {
   source = "git::https://github.com/danny-keizer/terraform-modules.git//Modules//Local_Files"
   file_name = "DEV"
