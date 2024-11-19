fd12650842699c1b75d45f29c0dee0b750f873b7
Danny <85065163+danny-keizer@users.noreply.github.com>
2024-11-19 17:01:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-terraform { 
+terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "4.10.0"
     }
-  }
-terraform { 
   cloud {  
     organization = "private-acme" 
     workspaces { 
